845eb9ba5e5736de203b9db05a142a9b9f2f25f9
alstj2384 <ass91160@naver.com>
2025-10-30 20:00:47 +0900

    feat(ExceptionMessage): 로또 구매 관련 예외 메시지 추가

--- src/main/java/lotto/constants/ExceptionMessage.java
+++ src/main/java/lotto/constants/ExceptionMessage.java
@@ -4,6 +4,8 @@ public enum ExceptionMessage {
     INVALID_LOTTO_NUMBER_COUNT("로또 번호는 6개여야 합니다."),
     DUPLICATED_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다"),
     INVALID_LOTTO_NUMBER_RANCE("로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    INVALID_MONEY_UNIT("구매 금액은 %d 단위여야 합니다."),
+    INVALID_MONEY_RANCE("로또는 %d ~ %d 범위로 구매할 수 있습니다."),
     ;
 
     private static final String EXCEPTION_PREFIX = "[ERROR] ";
@@ -13,7 +15,7 @@ public enum ExceptionMessage {
         this.message = message;
     }
 
-    public String getMessage() {
-        return EXCEPTION_PREFIX + message;
+    public String getMessage(Object... args) {
+        return String.format(EXCEPTION_PREFIX + message, args);
     }
 }
