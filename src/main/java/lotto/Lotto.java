eda1c948a7e5c4142a12bb0a55b1372d69126e3f
alstj2384 <ass91160@naver.com>
2025-10-30 19:04:43 +0900

    feat(Lotto): 로또 클래스 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import lotto.constants.ExceptionMessage;
 
 public class Lotto {
+    private static final int LOTTO_MAX_RANGE = 45;
+    private static final int LOTTO_MIN_RANGE = 1;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,10 +16,37 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateIsDuplicated(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_COUNT.getMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateIsDuplicated(List<Integer> numbers) {
+        Set<Integer> uniqueValues = new HashSet<>(numbers);
+
+        if (uniqueValues.size() != numbers.size()) {
+            throw new IllegalArgumentException(ExceptionMessage.DUPLICATED_LOTTO_NUMBER.getMessage());
+        }
+    }
+
+    private void validateNumberRange(List<Integer> numbers) {
+        if (numbers.stream().anyMatch(this::validateNumberRange)) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_LOTTO_NUMBER_RANCE.getMessage());
+        }
+    }
+
+    private boolean validateNumberRange(Integer number) {
+        return number < LOTTO_MIN_RANGE || number > LOTTO_MAX_RANGE;
+    }
+
+    public List<Integer> getLottoNumbers() {
+        return numbers;
+    }
+
 }
