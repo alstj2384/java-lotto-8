08f816632e921e980c70a1a89ea5ea45033df916
alstj2384 <ass91160@naver.com>
2025-11-01 16:33:55 +0900

    refactor(LottoConstant): 각 클래스의 상수를 LottoConstant에서 보관하도록 변경

---
+++ src/main/java/lotto/domain/constants/LottoConstant.java
@@ -0,0 +1,10 @@
+package lotto.domain.constants;
+
+public class LottoConstant {
+    public static final int LOTTO_MIN_RANGE = 1;
+    public static final int LOTTO_MAX_RANGE = 45;
+    public static final int LOTTO_SIZE = 6;
+    public static final int LOTTO_PRICE = 1000;
+    public static final int MIN_LOTTO_COUNT = 1;
+    public static final int MAX_LOTTO_COUNT = 1000000;
+}
